75099b1cfda64354d97e45e5c01dcc101dd5719d
Vince Kurtz <vjkurtz@gmail.com>
2023-10-11 14:00:10 -0700

    add some customization to the LSTM policy

--- ppo_lstm/train_cart_pole.py
+++ ppo_lstm/train_cart_pole.py
@@ -9,11 +9,13 @@
 import numpy as np
 
 from sb3_contrib import RecurrentPPO
+from sb3_contrib.common.recurrent.policies import RecurrentActorCriticPolicy
 from stable_baselines3.common.env_util import make_vec_env
 
 vec_env = make_vec_env("InvertedPendulum-v4", n_envs=1)
 
-model = RecurrentPPO("MlpLstmPolicy", vec_env, verbose=1)
+model = RecurrentPPO(RecurrentActorCriticPolicy, vec_env, verbose=1,
+        policy_kwargs={"shared_lstm":False, "enable_critic_lstm":False})
 
 model.learn(total_timesteps=30000)
 
