7a60661bdf0b945b0fbac1d1e6b48b59d5604503
Vince Kurtz <vjkurtz@gmail.com>
2023-10-11 12:27:14 -0700

    set custom initial state

--- ppo/test_cart_pole.py
+++ ppo/test_cart_pole.py
@@ -7,6 +7,9 @@
 ##
 
 import gymnasium as gym
+import numpy as np
+import time
+
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 
@@ -16,9 +19,17 @@ vec_env = make_vec_env("InvertedPendulum-v4", n_envs=1)
 # Load the trained model
 model = PPO.load("cart_pole")
 
-obs = vec_env.reset()
-for i in range(500):
+# Hackily set the initial state
+vec_env.reset()
+q0 = np.array([0, 0.1])
+v0 = np.array([0, 0.2])
+vec_env.envs[0].env.env.env.env.set_state(q0, v0)
+obs, _, _, _ = vec_env.step(np.array([[0]]))
+
+# Run a little simulation
+for i in range(1000):
     action, _state = model.predict(obs, deterministic=True)
     obs, reward, done, info = vec_env.step(action)
     vec_env.render("human")
 
+    time.sleep(0.01)
