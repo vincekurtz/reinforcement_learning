94b3bd78e83e70b9c1da92b8896818340e9c8645
Vince Kurtz <vjkurtz@gmail.com>
2023-10-11 13:36:19 -0700

    use a custom policy network

--- ppo/train_cart_pole.py
+++ ppo/train_cart_pole.py
@@ -7,14 +7,17 @@
 ##
 
 import gymnasium as gym
+
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
+
+from policy_network import CustomActorCriticPolicy
 
 # Set up the environment (a.k.a. plant)
 vec_env = make_vec_env("InvertedPendulum-v4", n_envs=1)
 
 # set up the model (a.k.a. controller)
-model = PPO("MlpPolicy", vec_env, verbose=1)
+model = PPO(CustomActorCriticPolicy, vec_env, verbose=1)
 
 # Do the learning
 model.learn(total_timesteps=30000)
