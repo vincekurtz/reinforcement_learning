40408b00408d3a98e4395be4e4678e16111e48b2
Vince Kurtz <vjkurtz@gmail.com>
2023-10-24 13:56:26 -0700

    rough plot of switching surface for model with history

--- ppo/solution_plots_pendulum.py
+++ ppo/solution_plots_pendulum.py
@@ -37,20 +37,34 @@ def plot_pendulum_vector_field(n=25):
 def plot_learned_switching_surface(model):
     switching_model = model.policy.mlp_extractor.policy_net.chooser
 
-    # Sample a bunch of states
-    n = 40
+    # Sample a bunch of initial states
+    n = 50
     thetas = np.linspace(-np.pi, 2*np.pi, n)
-    theta_dots = np.linspace(-8, 8, n)
+    theta_dots = np.linspace(-6, 6, n)
 
-    for theta in thetas:
-        for theta_dot in theta_dots:
-            observation = np.array([np.cos(theta), np.sin(theta), theta_dot])
-            observation = torch.from_numpy(observation).float().to(model.device)
-            sigma = switching_model(observation).item()
+    # Simulate the system for a while to get a history of observations
+    for theta_init in thetas:
+        for theta_dot_init in theta_dots:
+            num_steps = 1
+            history = np.zeros((10, 3))
+            env = gym.make("Pendulum-v1")
+            env.reset()
+            env.unwrapped.state = np.array([theta_init, theta_dot_init])
+
+            for t in range(num_steps):
+                obs, _, _, _, _ = env.step([0])
+                history = np.roll(history, shift=1, axis=0)
+                history[0] = obs
+            final_state = env.unwrapped.state
+
+            with torch.no_grad():
+                observation = history.flatten()
+                observation = torch.from_numpy(observation).float().to(model.device)
+                sigma = switching_model(observation).item()
 
             # Plot a dot shaded by the value of sigma
-            plt.scatter(theta, theta_dot, color='blue', edgecolors=None, alpha=sigma/2)
-
+            #plt.scatter([theta_init], [theta_dot_init], color='r')
+            plt.scatter(final_state[0], final_state[1], color='blue', edgecolors=None, alpha=sigma/2)
 
 if __name__=="__main__":
     # Plot the vector field for the pendulum
