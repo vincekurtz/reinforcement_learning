6f2665538cae5ec819dc63e904e4d1070c92d108
Vince Kurtz <vjkurtz@gmail.com>
2023-10-25 10:41:30 -0700

    clean up switching surface plot

--- solution_plots_pendulum.py
+++ solution_plots_pendulum.py
@@ -34,37 +34,41 @@ def plot_pendulum_vector_field(n=25):
     plt.xlabel(r"$\theta$")
     plt.ylabel(r"$\dot{\theta}$")
 
-def plot_learned_switching_surface(model):
-    switching_model = model.policy.mlp_extractor.policy_net.chooser
+@torch.no_grad
+def plot_switching_surface(model):
+    """
+    Make a contour plot of the switching surface. Assumes that the model was
+    trained with history_length=1 (i.e., no history).
+    """
+    def compute_switching_coefficient(obs):
+        """
+        A little helper function to compute the value for a given observation.
+        Handles conversion between torch and numpy.
+        """
+        obs = torch.from_numpy(obs).float().to(model.device)
+        sigma = model.policy.mlp_extractor.policy_net.chooser(obs)
+        return sigma.cpu().numpy()
 
     # Sample a bunch of initial states
-    n = 50
+    n = 150
     thetas = np.linspace(-np.pi, 2*np.pi, n)
-    theta_dots = np.linspace(-6, 6, n)
+    theta_dots = np.linspace(-8, 8, n)
 
-    # Simulate the system for a while to get a history of observations
-    for theta_init in thetas:
-        for theta_dot_init in theta_dots:
-            num_steps = 1
-            history = np.zeros((10, 3))
-            env = gym.make("Pendulum-v1")
-            env.reset()
-            env.unwrapped.state = np.array([theta_init, theta_dot_init])
+    # Set up a grid of observations
+    theta_grid, theta_dot_grid = np.meshgrid(thetas, theta_dots)
+    obs = np.zeros((n*n, 3))
+    obs[:,0] = np.cos(theta_grid.flatten())
+    obs[:,1] = np.sin(theta_grid.flatten())
+    obs[:,2] = theta_dot_grid.flatten()
 
-            for t in range(num_steps):
-                obs, _, _, _, _ = env.step([0])
-                history = np.roll(history, shift=1, axis=0)
-                history[0] = obs
-            final_state = env.unwrapped.state
+    # Compute the switching coefficient for each observation
+    sigmas = compute_switching_coefficient(obs)[:,0]
+    sigma_grid = sigmas.reshape((n,n))
 
-            with torch.no_grad():
-                observation = history.flatten()
-                observation = torch.from_numpy(observation).float().to(model.device)
-                sigma = switching_model(observation).cpu().numpy()
-
-            # Plot a dot shaded by the value of sigma
-            #plt.scatter([theta_init], [theta_dot_init], color='r')
-            plt.scatter(final_state[0], final_state[1], color='blue', edgecolors=None, alpha=sigma[1]/2)
+    # Plot the switching surface
+    plt.contourf(theta_grid, theta_dot_grid, sigma_grid, 20, cmap='binary', 
+                 alpha=0.7)
+    plt.colorbar(label="Switching coefficient")
 
 @torch.no_grad
 def plot_value_function(model):
@@ -100,19 +104,12 @@ def plot_value_function(model):
 
     # Plot the value function
     plt.contourf(theta_grid, theta_dot_grid, value_grid, 20, cmap='RdYlBu')
-
-    # Add a little scale bar with label
     plt.colorbar(label="Value")
-
-
 
 if __name__=="__main__":
     model = PPO.load("trained_models/pendulum")
-    plot_value_function(model)
+    #plot_value_function(model)
+    plot_switching_surface(model)
     plot_pendulum_vector_field()
-
-    # Plot the switching surface for the learned policy
-    #plot_learned_switching_surface(model)
-
 
     plt.show()
