57b9c362a2e0acac44e90ed0d32fa8529093c796
Vince Kurtz <vjkurtz@gmail.com>
2023-10-25 15:56:40 -0700

    swimmer discovers periodic 'gait' with history, but doesn't without

--- swimmer.py
+++ swimmer.py
@@ -2,9 +2,7 @@
 
 ##
 #
-# Train or test a policy with standard PPO on a simple cart-pole example. The
-# goal is just to stabilize the cart-pole around the upright. No swing-up is
-# required.
+# Train or test a policy with standard PPO on a little swimmer robot.
 #
 ##
 
@@ -25,8 +23,8 @@ def make_environment(render_mode=None):
     testing.
     """
     return make_vec_env(EnvWithObservationHistory, n_envs=1,
-                        env_kwargs={"env_name": "InvertedPendulum-v4", 
-                                    "history_length": 1,
+                        env_kwargs={"env_name": "Swimmer-v4", 
+                                    "history_length": 10,
                                     "render_mode": render_mode})
 
 def train():
@@ -37,25 +35,27 @@ def train():
     
     # set up the model (a.k.a. controller)
     model = PPO(KoopmanPolicy, vec_env, gamma=0.98, learning_rate=1e-3, 
-                tensorboard_log="/tmp/cart_pole_tensorboard/",
-                verbose=1, policy_kwargs={"num_linear_systems": 1})
+                tensorboard_log="/tmp/swimmer_tensorboard/",
+                verbose=1, policy_kwargs={"num_linear_systems": 2})
+    #model = PPO('MlpPolicy', vec_env, verbose=1, 
+    #            tensorboard_log="/tmp/swimmer_tensorboard/")
 
     # Print how many parameters this thing has
     num_params = sum(p.numel() for p in model.policy.parameters())
-    print("Training a policy with {num_params} parameters")
+    print(f"Training a policy with {num_params} parameters")
 
     # Do the learning
     model.learn(total_timesteps=200_000)
 
     # Save the model
-    model.save("trained_models/cart_pole")
+    model.save("trained_models/swimmer")
 
 def test():
     """
     Load the trained model from disk and run a little simulation
     """
     vec_env = make_environment(render_mode="human")
-    model = PPO.load("trained_models/cart_pole")
+    model = PPO.load("trained_models/swimmer")
 
     obs = vec_env.reset()
     for i in range(1000):
@@ -66,7 +66,7 @@ def test():
 if __name__=="__main__":
     # Must run with --train or --test
     if len(sys.argv) != 2 or sys.argv[1] not in ["--train", "--test"]:
-        print("Usage: python cart_pole.py [--train, --test]")
+        print("Usage: python swimmer.py [--train, --test]")
         sys.exit(1)
 
     if sys.argv[1] == "--train":
