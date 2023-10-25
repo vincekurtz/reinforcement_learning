3ffbeb13a67258be8eea80bf52315c34dc0a5d5a
Vince Kurtz <vjkurtz@gmail.com>
2023-10-25 14:55:41 -0700

    use >2 linear subsystems

--- policies.py
+++ policies.py
@@ -2,81 +2,6 @@ import torch
 from torch import nn
 from stable_baselines3.common.policies import ActorCriticPolicy
 
-class Quadratic(nn.Module):
-    """
-    A simple quadratic function
-
-        y = x'Ax + b'x + c
-
-    where A, b, and c are learned parameters.
-    """
-    def __init__(self, input_size):
-        super().__init__()
-        self.input_size = input_size
-
-        self.A = nn.Parameter(torch.randn(input_size, input_size), requires_grad=True)
-        self.b = nn.Parameter(torch.randn(input_size), requires_grad=True)
-        self.c = nn.Parameter(torch.randn(1), requires_grad=True)
-
-    def forward(self, x):
-        y = (x @ self.A @ x.T).diag() + x @ self.b + self.c
-        return y.view(-1, 1)
-
-class KoopmanNetwork(nn.Module):
-    """
-    A neural net that maps from a history of observations to control actions.
-    Does so by building several linear maps, each of which is based on a learned
-    Koopman linearization around a different fixed point of the system.
-    """
-    def __init__(self, input_size, output_size):
-        super().__init__()
-        self.input_size = input_size
-        self.output_size = output_size
-
-        # Linear mapping from input (history of observations) to output (control)
-        self.linear_system1 = nn.Linear(input_size, output_size, bias=False)
-
-        # Second linear mapping from input to output, hopefully to capture a
-        # Koopman linearization around a different equilibrium
-        self.linear_system2 = nn.Linear(input_size, output_size, bias=False)
-
-        # The chooser maps the input to a number between 0 and 1, which is used
-        # to decide which linear system to use
-        self.chooser = nn.Sequential(
-            nn.Linear(input_size, 64), nn.ReLU(),
-            nn.Linear(64, 2), nn.Sigmoid()
-        )
-        #self.chooser = nn.Sequential(
-        #    nn.Linear(input_size, 2, bias=False), 
-        #    nn.Sigmoid()
-        #)
-
-    def forward(self, observations):
-        u1 = self.linear_system1(observations)
-        u2 = self.linear_system2(observations)
-        sigma = self.chooser(observations)
-        return sigma[:,0:1]*u1 + sigma[:,1:2]*u2
-    
-class PiecewiseQuadratic(nn.Module):
-    def __init__(self, input_size):
-        super().__init__()
-        self.input_size = input_size
-        self.output_size = 1
-
-        self.quadratic1 = Quadratic(input_size)
-        self.quadratic2 = Quadratic(input_size)
-
-        self.chooser = nn.Sequential(
-            nn.Linear(input_size, 64), nn.ReLU(),
-            nn.Linear(64, 2), nn.Sigmoid()
-        )
-
-    def forward(self, x):
-        y1 = self.quadratic1(x)
-        y2 = self.quadratic2(x)
-        sigma = self.chooser(x)
-        return sigma[:,0:1]*y1 + sigma[:,1:2]*y2
-
 class KoopmanMlpExtractor(nn.Module):
     """
     A custom neural net for both the policy and the value function.
@@ -91,34 +16,42 @@ class KoopmanMlpExtractor(nn.Module):
         self.latent_dim_pi = output_size
         self.latent_dim_vf = 1
 
-        # Policy is piecewise linear
-        self.linear_system1 = nn.Linear(input_size, output_size, bias=False)
-        self.linear_system2 = nn.Linear(input_size, output_size, bias=False)
-
-        # Value function is piecewise quadratic
-        self.quadratic1 = Quadratic(input_size)
-        self.quadratic2 = Quadratic(input_size)
+        # Policy is piecewise linear with a learned switching surface
+        self.num_linear_systems = 3
+        
+        self.linear_systems = nn.ModuleList([
+            nn.Linear(input_size, output_size, bias=False)
+            for _ in range(self.num_linear_systems)])
 
         # We define a switching surface between regimes with a neural net. This
-        # network outputs a number between 0 and 1
+        # network outputs scores in [0,1] that determine which linear layer is active
         self.chooser = nn.Sequential(
+            nn.Linear(input_size, self.num_linear_systems, bias=False), 
+            nn.Sigmoid())
+        
+        # Value function is a simple MLP
+        self.value_net = nn.Sequential(
             nn.Linear(input_size, 64), nn.ReLU(),
-            nn.Linear(64, 2), nn.Sigmoid())
+            nn.Linear(64, self.latent_dim_vf))
 
     def forward(self, x):
         return self.forward_actor(x), self.forward_critic(x)
 
     def forward_actor(self, x):
-        y1 = self.linear_system1(x)
-        y2 = self.linear_system2(x)
-        sigma = self.chooser(x)
-        return sigma[:,0:1]*y1 + sigma[:,1:2]*y2
+        # Compute the switching coefficients
+        sigma = self.chooser(x)   # shape: [batch_size, num_linear_systems]
+
+        # Pass input through each linear system
+        output = [linear(x) for linear in self.linear_systems]
+        output = torch.stack(output, dim=1)  # shape: [batch_size, num_linear_systems, output_size]
+
+        # Weight the outputs by the switching coefficients
+        output = (sigma.unsqueeze(-1) * output).sum(dim=1)  # shape: [batch_size, output_size]
+
+        return output
 
     def forward_critic(self, x):
-        y1 = self.quadratic1(x)
-        y2 = self.quadratic2(x)
-        sigma = self.chooser(x)
-        return sigma[:,0:1]*y1 + sigma[:,1:2]*y2
+        return self.value_net(x)
 
 class KoopmanPolicy(ActorCriticPolicy):
     """
