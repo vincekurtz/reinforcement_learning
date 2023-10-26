db63f91d6c83774ac82326d5eeab508cc17cb6b5
Vince Kurtz <vjkurtz@gmail.com>
2023-10-26 11:14:57 -0700

    cleanup

--- envs.py
+++ envs.py
@@ -14,8 +14,8 @@ class HistoryWrapper(gym.ObservationWrapper, gym.utils.RecordConstructorArgs):
     instead of just the current one. Similar to the `FrameStack` wrapper, but 
     the buffer starts with zeros rather than copies of the initial observation.
 
-    Note: if the original observation is of size (N,), then the wrapped
-    observation will be of size, (N*history_length,).
+    If the original observation is of size (N,), then the wrapped observation
+    will be of size, (N*history_length,).
     """
     def __init__(self, env, history_length):
         """
@@ -37,9 +37,9 @@ class HistoryWrapper(gym.ObservationWrapper, gym.utils.RecordConstructorArgs):
         self.frames = deque(maxlen=history_length)
 
         low = np.repeat(self.observation_space.low[None, ...], 
-                        history_length, axis=0)
+                        history_length, axis=0).flatten()
         high = np.repeat(self.observation_space.high[None, ...], history_length, 
-                         axis=0)
+                         axis=0).flatten()
         self.observation_space = gym.spaces.Box(
             low=low, high=high, dtype=self.observation_space.dtype)
         
@@ -57,54 +57,3 @@ class HistoryWrapper(gym.ObservationWrapper, gym.utils.RecordConstructorArgs):
     
     def _get_obs(self):
         return np.asarray(self.frames).flatten()
-
-class EnvWithObservationHistory(gym.Env):
-    """
-    A simple gym environment wrapper that outputs a history of observations
-    instead of just the current one.
-    """
-    def __init__(self, env_name, history_length, render_mode=None):
-        super().__init__()
-        self.history_length = history_length
-        self.env = gym.make(env_name, render_mode=render_mode)
-
-        # The environment must have vector observations
-        assert isinstance(self.env.observation_space, gym.spaces.Box)
-        assert len(self.env.observation_space.shape) == 1
-
-        assert history_length ==1, "debug"
-        
-        self.observation_size = self.env.observation_space.shape[0]
-        self.observation_space = gym.spaces.Box(
-            low=-np.inf, high=np.inf, 
-            shape=(self.observation_size*self.history_length,), 
-            dtype=np.float32
-        )
-        self.action_space = self.env.action_space
-
-    def reset(self, **kwargs):
-        self.history = np.zeros((self.history_length, self.observation_size))
-        obs, info = self.env.reset(**kwargs)
-        self.history[0, :] = obs
-        return obs, info
-
-    def step(self, action):
-        obs, reward, terminated, truncated, info = self.env.step(action)
-        self.history = np.roll(self.history, shift=1, axis=0)
-        self.history[0, :] = obs
-        return obs, reward, terminated, truncated, info
-
-    def _get_obs(self):
-        return self.history.flatten()
-
-if __name__=="__main__":
-    # run some tests
-    original = gym.make("Swimmer-v4")
-    wrapped = HistoryWrapper(original, 3)
-
-    obs, info = wrapped.reset()
-    print(obs.T)
-
-    for t in range(5):
-        obs, _, _, _, _ = wrapped.step(wrapped.action_space.sample())
-        print(obs.T)
--- swimmer.py
+++ swimmer.py
@@ -8,7 +8,6 @@
 
 import sys
 from stable_baselines3 import PPO
-from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.utils import set_random_seed
 
 from stable_baselines3.common.vec_env import VecNormalize, DummyVecEnv
@@ -16,31 +15,28 @@ from stable_baselines3.common.monitor import Monitor
 from sb3_contrib.common.wrappers import TimeFeatureWrapper
 
 from policies import KoopmanPolicy
-from envs import EnvWithObservationHistory, HistoryWrapper
+from envs import HistoryWrapper
 import gymnasium as gym
+
+# Whether to run the baseline MLP implementation from stable-baselines3 rl zoo
+MLP_BASELINE = True
 
 # Try to make things deterministic
 SEED = 1
 set_random_seed(SEED, using_cuda=True)
 
-def make_environment(render_mode=None):
-    vec_env = make_vec_env("Swimmer-v4", n_envs=1,
-                        seed=SEED,
-                        wrapper_class=TimeFeatureWrapper,
-                        env_kwargs={"render_mode": render_mode})
-    vec_env = VecNormalize(vec_env, norm_obs=True, norm_reward=False)
-    return vec_env
-
-def make_environment2(render_mode=None):
+def make_environment(render_mode=None, test_mode=False):
     """
     Set up the gym environment (a.k.a. plant). Used for both training and
     testing.
     """
     env = gym.make("Swimmer-v4", render_mode=render_mode)
     env.action_space.seed(SEED)
+    if MLP_BASELINE:
+        env = TimeFeatureWrapper(env, test_mode=test_mode)
+    else:
+        env = HistoryWrapper(env, 20)
     env = Monitor(env)
-    env = TimeFeatureWrapper(env)
-    env = HistoryWrapper(env, 1)
     vec_env = DummyVecEnv([lambda: env])
     vec_env.seed(SEED)
 
@@ -51,21 +47,23 @@ def train():
     """
     Train the model with PPO and save it to disk.
     """
-    vec_env = make_environment2() 
+    vec_env = make_environment() 
     
     # set up the model (a.k.a. controller)
-    model = PPO(KoopmanPolicy, vec_env, gamma=0.9999,
-                tensorboard_log="/tmp/swimmer_tensorboard/",
-                verbose=1, policy_kwargs={"num_linear_systems": 2})
-    #model = PPO('MlpPolicy', vec_env, verbose=1, gamma=0.9999,
-    #            tensorboard_log="/tmp/swimmer_tensorboard/")
+    if MLP_BASELINE:
+        model = PPO('MlpPolicy', vec_env, verbose=1, gamma=0.9999,
+                    tensorboard_log="/tmp/swimmer_tensorboard/")
+    else:
+        model = PPO(KoopmanPolicy, vec_env, gamma=0.9999,
+                    tensorboard_log="/tmp/swimmer_tensorboard/",
+                    verbose=1, policy_kwargs={"num_linear_systems": 2})
 
     # Print how many parameters this thing has
     num_params = sum(p.numel() for p in model.policy.parameters())
     print(f"Training a policy with {num_params} parameters")
 
     # Do the learning
-    model.learn(total_timesteps=500_000)
+    model.learn(total_timesteps=1_000_000)
 
     # Save the model
     model.save("trained_models/swimmer")
@@ -74,7 +72,7 @@ def test():
     """
     Load the trained model from disk and run a little simulation
     """
-    vec_env = make_environment(render_mode="human")
+    vec_env = make_environment(render_mode="human", test_mode=True)
     model = PPO.load("trained_models/swimmer")
 
     obs = vec_env.reset()
