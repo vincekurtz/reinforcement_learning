6aa39a8e93e7843159f9061fb1168718b590bf78
Vince Kurtz <vjkurtz@gmail.com>
2023-10-26 10:02:22 -0700

    set random seed more carefully for vec_env

--- envs.py
+++ envs.py
@@ -4,8 +4,59 @@
 #
 ##
 
+from collections import deque
 import gymnasium as gym
 import numpy as np
+
+class HistoryWrapper(gym.ObservationWrapper, gym.utils.RecordConstructorArgs):
+    """
+    A simple gym observation wrapper that outputs a history of observations
+    instead of just the current one. Similar to the `FrameStack` wrapper, but 
+    the buffer starts with zeros rather than copies of the initial observation.
+
+    Note: if the original observation is of size (N,), then the wrapped
+    observation will be of size, (N*history_length,).
+    """
+    def __init__(self, env, history_length):
+        """
+        Observation wrapper that holds a rolling history of past observations.
+
+        Args:
+            env: The environment to wrap.
+            history_length: The length of the observation history.
+        """
+        # The environment must have vector observations
+        assert isinstance(env.observation_space, gym.spaces.Box)    
+        assert len(env.observation_space.shape) == 1
+
+        gym.utils.RecordConstructorArgs.__init__(self, 
+                                                 history_length=history_length)
+        gym.ObservationWrapper.__init__(self, env)
+
+        self.history_length = history_length
+        self.frames = deque(maxlen=history_length)
+
+        low = np.repeat(self.observation_space.low[None, ...], 
+                        history_length, axis=0)
+        high = np.repeat(self.observation_space.high[None, ...], history_length, 
+                         axis=0)
+        self.observation_space = gym.spaces.Box(
+            low=low, high=high, dtype=self.observation_space.dtype)
+        
+    def step(self, action):
+        obs, reward, terminated, truncated, info = self.env.step(action)
+        self.frames.append(obs)
+        return self._get_obs(), reward, terminated, truncated, info
+    
+    def reset(self, **kwargs):
+        obs, info = self.env.reset(**kwargs)
+        for _ in range(self.history_length):
+            self.frames.append(np.zeros_like(obs))
+        self.frames.append(obs)
+        return self._get_obs(), info
+    
+    def _get_obs(self):
+        return np.asarray(self.frames).flatten()
 
 class EnvWithObservationHistory(gym.Env):
     """
@@ -13,12 +64,15 @@ class EnvWithObservationHistory(gym.Env):
     instead of just the current one.
     """
     def __init__(self, env_name, history_length, render_mode=None):
+        super().__init__()
         self.history_length = history_length
         self.env = gym.make(env_name, render_mode=render_mode)
 
         # The environment must have vector observations
         assert isinstance(self.env.observation_space, gym.spaces.Box)
         assert len(self.env.observation_space.shape) == 1
+
+        assert history_length ==1, "debug"
         
         self.observation_size = self.env.observation_space.shape[0]
         self.observation_space = gym.spaces.Box(
@@ -31,14 +85,26 @@ class EnvWithObservationHistory(gym.Env):
     def reset(self, **kwargs):
         self.history = np.zeros((self.history_length, self.observation_size))
         obs, info = self.env.reset(**kwargs)
-        self.history[0] = obs
-        return self._get_obs(), info
+        self.history[0, :] = obs
+        return obs, info
 
     def step(self, action):
         obs, reward, terminated, truncated, info = self.env.step(action)
         self.history = np.roll(self.history, shift=1, axis=0)
-        self.history[0] = obs
-        return self._get_obs(), reward, terminated, truncated, info
+        self.history[0, :] = obs
+        return obs, reward, terminated, truncated, info
 
     def _get_obs(self):
         return self.history.flatten()
+
+if __name__=="__main__":
+    # run some tests
+    original = gym.make("Swimmer-v4")
+    wrapped = HistoryWrapper(original, 3)
+
+    obs, info = wrapped.reset()
+    print(obs.T)
+
+    for t in range(5):
+        obs, _, _, _, _ = wrapped.step(wrapped.action_space.sample())
+        print(obs.T)
--- swimmer.py
+++ swimmer.py
@@ -11,27 +11,38 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.utils import set_random_seed
 
-from stable_baselines3.common.vec_env import VecNormalize
+from stable_baselines3.common.vec_env import VecNormalize, DummyVecEnv
+from stable_baselines3.common.monitor import Monitor
 from sb3_contrib.common.wrappers import TimeFeatureWrapper
 
 from policies import KoopmanPolicy
-from envs import EnvWithObservationHistory
+from envs import EnvWithObservationHistory, HistoryWrapper
+import gymnasium as gym
 
 # Try to make things deterministic
-set_random_seed(1, using_cuda=True)
+SEED = 1
+set_random_seed(SEED, using_cuda=True)
 
 def make_environment(render_mode=None):
+    vec_env = make_vec_env("Swimmer-v4", n_envs=1,
+                        seed=SEED,
+                        wrapper_class=TimeFeatureWrapper,
+                        env_kwargs={"render_mode": render_mode})
+    vec_env = VecNormalize(vec_env, norm_obs=True, norm_reward=False)
+    return vec_env
+
+def make_environment2(render_mode=None):
     """
     Set up the gym environment (a.k.a. plant). Used for both training and
     testing.
     """
-    #return make_vec_env(EnvWithObservationHistory, n_envs=1,
-    #                    env_kwargs={"env_name": "Swimmer-v4", 
-    #                                "history_length": 32,
-    #                                "render_mode": render_mode})
-    vec_env = make_vec_env("Swimmer-v4", n_envs=1,
-                        wrapper_class=TimeFeatureWrapper,
-                        env_kwargs={"render_mode": render_mode})
+    env = gym.make("Swimmer-v4", render_mode=render_mode)
+    env.action_space.seed(SEED)
+    env = Monitor(env)
+    env = TimeFeatureWrapper(env)
+    vec_env = DummyVecEnv([lambda: env])
+    vec_env.seed(SEED)
+
     vec_env = VecNormalize(vec_env, norm_obs=True, norm_reward=False)
     return vec_env
 
@@ -39,21 +50,21 @@ def train():
     """
     Train the model with PPO and save it to disk.
     """
-    vec_env = make_environment() 
+    vec_env = make_environment2() 
     
     # set up the model (a.k.a. controller)
-    #model = PPO(KoopmanPolicy, vec_env, gamma=0.9999,
-    #            tensorboard_log="/tmp/swimmer_tensorboard/",
-    #            verbose=1, policy_kwargs={"num_linear_systems": 8})
-    model = PPO('MlpPolicy', vec_env, verbose=1, gamma=0.9999,
-                tensorboard_log="/tmp/swimmer_tensorboard/")
+    model = PPO(KoopmanPolicy, vec_env, gamma=0.9999,
+                tensorboard_log="/tmp/swimmer_tensorboard/",
+                verbose=1, policy_kwargs={"num_linear_systems": 2})
+    #model = PPO('MlpPolicy', vec_env, verbose=1, gamma=0.9999,
+    #            tensorboard_log="/tmp/swimmer_tensorboard/")
 
     # Print how many parameters this thing has
     num_params = sum(p.numel() for p in model.policy.parameters())
     print(f"Training a policy with {num_params} parameters")
 
     # Do the learning
-    model.learn(total_timesteps=1_000_000)
+    model.learn(total_timesteps=500_000)
 
     # Save the model
     model.save("trained_models/swimmer")
@@ -72,6 +83,12 @@ def test():
         vec_env.render("human")
 
 if __name__=="__main__":
+    vec_env1 = make_environment()
+    vec_env2 = make_environment2()
+
+    print(vec_env1.envs[0])
+    print(vec_env2.envs[0])
+
     # Must run with --train or --test
     if len(sys.argv) != 2 or sys.argv[1] not in ["--train", "--test"]:
         print("Usage: python swimmer.py [--train, --test]")
