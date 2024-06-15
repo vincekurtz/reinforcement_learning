ab89a1386d57a42fd640d065975d0372c68557a8
Vince Kurtz <vjkurtz@gmail.com>
2024-06-15 13:27:51 -0700

    add humanoid bps example

---
+++ examples/humanoid_standup_bps.py
@@ -0,0 +1,75 @@
+import pickle
+import sys
+
+import jax
+
+from playground.architectures import MLP
+from playground.boltzmann import (
+    BoltzmannPolicySearch,
+    BoltzmannPolicySearchOptions,
+)
+from playground.envs.humanoid.standup import HumanoidStandupEnv
+from playground.simulation import run_interactive
+
+"""
+Use Boltzmann Policy Search to train a humanoid standup task.
+"""
+
+
+def train():
+    """Train the policy and save it to a file."""
+    env = HumanoidStandupEnv()
+    policy_net = MLP(layer_sizes=(32, 32, 17))
+    options = BoltzmannPolicySearchOptions(
+        episode_length=100,
+        num_envs=512,
+        temperature=1.0,
+        sigma=0.1,
+    )
+
+    bps = BoltzmannPolicySearch(
+        env=env,
+        policy=policy_net,
+        options=options,
+        tensorboard_logdir="/tmp/rl_playground/humanoid_bps",
+    )
+    params = bps.train(iterations=2000, num_evals=10)
+
+    fname = "/tmp/humanoid_bps.pkl"
+    print(f"Saving policy to {fname}...")
+    with open(fname, "wb") as f:
+        data = {"params": params, "network": policy_net}
+        pickle.dump(data, f)
+
+
+def test():
+    """Test the policy with an interactive mujoco simulation."""
+    env = HumanoidStandupEnv()
+
+    # Load the trained policy
+    with open("/tmp/humanoid_bps.pkl", "rb") as f:
+        data = pickle.load(f)
+    policy_net = data["network"]
+    params = data["params"]
+
+    # Create a policy function
+    jit_policy = jax.jit(lambda obs: policy_net.apply(params, obs))
+
+    # Run the sim
+    run_interactive(env, jit_policy, fixed_camera_id=None)
+
+
+if __name__ == "__main__":
+    usage_message = "Usage: python humanoid_standup_bps.py [train|test]"
+
+    if len(sys.argv) != 2:
+        print(usage_message)
+        sys.exit(1)
+
+    if sys.argv[1] == "train":
+        train()
+    elif sys.argv[1] == "test":
+        test()
+    else:
+        print(usage_message)
+        sys.exit(1)
